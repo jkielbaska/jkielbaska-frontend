58152a016cf486cb02abfe30f53916b443fd4806
Jakub <kielbaskakuba@gmail.com>
2023-10-21 15:24:16 +0200

    Add redux-persist persistConifig, persistedReducer to store. Add PersistGate to root

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { todosSlice } from "./slices/todoSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = { key: "root", version: 1, storage: storage };
+const persistedReducer = persistReducer(persistConfig, todosSlice.reducer);
 
 export const store = configureStore({
   reducer: {
-    todos: todosSlice.reducer,
+    todos: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
-
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
